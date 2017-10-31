246b6cd24f13dc3c7333fd996e3bb6090ba34ac6
Sdharma <sdharmait2006@gmail.com>
2017-10-31 16:03:11 +0800

    Initial commit

---
+++ GAuth-master/Tests/StepDefinitions/Google_OAuth.cs
@@ -0,0 +1,124 @@
+﻿using Google.Apis.Tasks.v1;
+using Google.Apis.Tasks.v1.Data;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace Tests
+{
+    [Binding]
+    public sealed class Google_OAuth
+    {
+        // For additional details on SpecFlow step definitions see http://go.specflow.org/doc-stepdef
+        TasksService service;
+        private static String tasklistid = "";
+        private static String tasktomove = "";
+        private static String title;
+        private static TaskList taskList;
+        //static HttpURLConnection conn;
+
+        [Given(@"a valid google user is logged in")]
+        public void GivenAValidGoogleUserIsLoggedIn()
+        {
+            service = OAuth.getTasksService();
+        }
+
+        [When(@"he requested task ""(.*)"" creation under taskList ""(.*)""")]
+        public void WhenHeRequestedTaskCreationUnderTaskList(string taskTitle, string taskListName)
+        {
+            title = taskTitle;
+            taskList = getTaskID(taskListName);
+        }
+
+        [Then(@"the task should be created")]
+        public void ThenTheTaskShouldBeCreated()
+        {
+            try
+            {
+                OAuth.CreateTaskService(service, taskList.Id, title);
+            }
+            catch (IOException e)
+            {
+                // TODO Auto-generated catch block
+                e.StackTrace.ToString();
+            }
+        }
+
+        [When(@"he requested to move the task ""(.*)"" under taskList ""(.*)""")]
+        public void WhenHeRequestedToMoveTheTaskUnderTaskList(string taskTitle, string taskListName)
+        {
+            title = taskTitle;
+            taskList = getTaskID(taskListName);
+            try
+            {
+                OAuth.gettaskslistService(service, taskList.Id, taskTitle);
+            }
+            catch (IOException e)
+            {
+                // TODO Auto-generated catch block
+                e.StackTrace.ToString();
+            }
+        }
+
+        [Then(@"the task should be moved")]
+        public void ThenTheTaskShouldBeMoved()
+        {
+            try
+            {
+                OAuth.movetasksService(service, taskList.Id);
+            }
+            catch (IOException e)
+            {
+                // TODO Auto-generated catch block
+                e.StackTrace.ToString();
+            }
+        }
+
+        public TaskList getTaskID(String taskListName)
+        {
+
+            // Print the first 10 task lists.
+            TaskLists result = new TaskLists();
+            TaskList resultList = new TaskList();
+            try
+            {
+                result = service.Tasklists.List().Execute();
+                     
+            }
+            catch (IOException e1)
+            {
+                // TODO Auto-generated catch block
+                e1.StackTrace.ToString();
+            }
+
+            List<TaskList> tasklists = result.Items.ToList();
+            if (tasklists == null || tasklists.Count == 0)
+            {
+                Console.WriteLine("No task lists found.");
+            }
+            else
+            {
+                Console.WriteLine("Task lists:");
+                foreach (TaskList tasklist in tasklists)
+                {
+                    Console.WriteLine("%s (%s)\n",
+                            tasklist.Title,
+                            tasklist.Id);
+
+                    if (tasklist.Title.ToLower().Equals(taskListName))
+                    {
+                        resultList = tasklist;
+                        tasklistid = tasklist.Id;
+                        break;
+                    }
+
+                }
+            }
+            return resultList;
+        }
+
+    }
+}
---
+++ GAuth-master/Tests/class/OAuth.cs
@@ -0,0 +1,102 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Tasks.v1;
+using Google.Apis.Tasks.v1.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace Tests
+{
+    class OAuth
+    {
+        // If modifying these scopes, delete your previously saved credentials
+        // at ~/.credentials/tasks-dotnet-quickstart.json
+        static string[] Scopes = { TasksService.Scope.Tasks };
+        static string ApplicationName = "Google Tasks API .NET Quickstart";
+        private static String tasklistid = "";
+        private static String tasktomove = "";
+        
+        public static UserCredential authorize()
+        {
+            UserCredential credential;
+            using (var stream =
+                new FileStream("C:\\Users\\sup-dmsvel\\Downloads\\GAuth-master\\GAuth-master\\Tests\\bin\\Debug\\client_secret.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = System.Environment.GetFolderPath(
+                    System.Environment.SpecialFolder.Personal);
+                credPath = Path.Combine(credPath, ".credentials/tasks-dotnet-quickstart.json");
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+            return credential;
+        }
+        public static TasksService getTasksService()
+        {
+            UserCredential credential = authorize();
+            //return new Tasks
+            // Create Google Tasks API service.
+           var service = new TasksService(new BaseClientService.Initializer()
+           {
+               HttpClientInitializer = credential,
+               ApplicationName = ApplicationName,
+           });
+
+           
+            return service;
+        }
+
+        public static void gettaskslistService(TasksService service, string strtaskid, string taskTitle) 
+        {
+          Tasks result1 = service.Tasks.List(strtaskid).Execute();
+            List<Google.Apis.Tasks.v1.Data.Task> tasks1 = result1.Items.ToList();   	   
+	   	 if (tasks1 == null || tasks1.Count == 0) {
+               Console.WriteLine("No task lists found.");
+            } else {
+                Console.WriteLine("New Added Task lists:");
+                foreach (Google.Apis.Tasks.v1.Data.Task tasklist in tasks1)
+                {
+                    Console.WriteLine("%s (%s)  (%s)\n",
+                            tasklist.Title,
+                            tasklist.Id,
+                            tasklist.Position);
+                    if (tasklist.Title.Contains(taskTitle))
+                    {
+                        tasktomove = tasklist.Id;
+                        Console.WriteLine("Tast to Move:" + tasktomove + "\nTitle:" + tasklist.Title + "\nPosition:" + tasklist.Position);
+                        break;
+                    }
+                }
+            }
+
+        }
+        public static void CreateTaskService(TasksService service, string strtaskid, string strTaskName) 
+        {
+            Google.Apis.Tasks.v1.Data.Task task = new Google.Apis.Tasks.v1.Data.Task();
+            task.Title = strTaskName;
+            Google.Apis.Tasks.v1.Data.Task result = service.Tasks.Insert(task,"@default").Execute();
+            Console.WriteLine(result.Title);
+            Console.WriteLine("New Task %s (%s)\n",
+                     result.Title,
+    			     result.Id);
+        }
+        public static void movetasksService(TasksService service, string taskListid) 
+        {         
+            Google.Apis.Tasks.v1.Data.Task result2 = service.Tasks.Move(taskListid, tasktomove).Execute();
+            Console.WriteLine("Moved Task Id :"+result2.Id+"\nTitle:" + result2.Title+ "\nPosition:" + result2.Position);
+        }
+    }
+   
+}
